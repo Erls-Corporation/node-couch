88f9588d23148d0f8b942ffd28b961e7f65d64b8
Guan Yang <guan@yang.dk>
2010-01-30 18:42:27 -0500

    Fixed to work with recent version of node: client[verb] -> client.request(verb, ...)

--- module/node-couch.js
+++ module/node-couch.js
@@ -55,10 +55,10 @@ function _interact(verb, path, successStatus, options, port, host) {
 			verb = "post";
 		}
 		var requestBody = toJSON(options.body);
-		request = client[verb](requestPath, [["Content-Length", requestBody.length], ["Content-Type", "application/json"]]);
+		request = client.request(verb.toUpperCase(), requestPath, [["Content-Length", requestBody.length], ["Content-Type", "application/json"]]);
 		request.sendBody(requestBody, "utf8");
 	} else {
-		request = client[verb](requestPath);
+		request = client.request(verb.toUpperCase(), requestPath);
 	}
 	request.finish(function(response) {
 		var responseBody = "";
